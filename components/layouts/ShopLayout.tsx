4f3c739c1ca0043c6f38893cf4238924dc473ffb
Daniel Restrepo <jdrestrepo@unitecnica.net>
2023-11-05 22:49:55 -0500

    Create meta tags shop layout

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,52 @@
+import Head from "next/head";
+import { FC, PropsWithChildren } from "react";
+
+interface Props {
+  title: string;
+  pageDescription: string;
+  imageFullUrl?: string;
+}
+
+export const ShopLayout: FC<PropsWithChildren<Props>> = ({
+  children,
+  title,
+  pageDescription,
+  imageFullUrl,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={ pageDescription }/>
+        
+        <meta name="og:title" content={ title } />
+        <meta name="og:description" content={ pageDescription } />
+        
+        {
+            imageFullUrl && (
+                <meta name="og:image" content={imageFullUrl}/>
+            )
+        }
+      </Head>
+      <nav>{/* TODO :Navbar */}</nav>
+      {/* TODO: Sidebar*/}
+
+      <main
+        style={{
+          margin: "80px auto",
+          maxWidth: "1440px",
+          padding: "0px 30px",
+        }}
+      >
+        {children}
+      </main>
+      {/* Footer */}
+
+      <footer>
+        {/* Custom Footer */}
+      </footer>
+    </>
+  );
+};
+
+export default ShopLayout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from 'next'
+import { Typography } from '@mui/material';
+import { ShopLayout } from '../components/layouts'
 
 const Home: NextPage = () => {
   return (
-    <h1>Clothes shopp</h1>
+    <ShopLayout title={'The Clothe´s Shop- Home'} pageDescription={'Encuentra las mejores prendas de vestir y ropa deportiva'}>
+      <Typography variant='h1' component='h1'>Tienda</Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+    </ShopLayout>
   )
 }
 

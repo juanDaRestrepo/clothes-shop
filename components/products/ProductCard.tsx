a1f947fbcc747341eb0992ef0a4d3e3136f32ef6
Daniel Restrepo <jdrestrepo@unitecnica.net>
2023-11-07 11:39:37 -0500

    Finish product card with images changing on hover

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,25 +1,47 @@
 
 
-import { Typography, Grid, Card, CardActionArea, CardMedia } from '@mui/material';
+import { Typography, Grid, Card, CardActionArea, CardMedia, Box } from '@mui/material';
 import { IProduct } from '../../interfaces';
-import { FC } from 'react';
+import { FC, useMemo, useState } from 'react';
 
 interface Props {
     product: IProduct;
 }
 
 const ProductCard: FC<Props> = ({product}) => {
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  const productImage = useMemo(() => {
+    return isHovered
+      ? `products/${ product.images[1] }`
+      : `products/${ product.images[0] }`
+  }, [isHovered, product.images])
+
   return (
-    <Grid item xs={6} sm={4} key={product.slug}>
+    <Grid 
+      item 
+      xs={6} 
+      sm={4} 
+      key={product.slug}
+      onMouseEnter={ () => setIsHovered(true) }
+      onMouseLeave={ () => setIsHovered(false) }
+    >
       <Card>
         <CardActionArea>
           <CardMedia
             component="img"
-            image={`products/${product.images[0]}`}
+            className='fadeIn'
+            image={productImage}
             alt={product.title}
           />
         </CardActionArea>
       </Card>
+
+      <Box sx={{mt: 1}} className='fadeIn'>
+          <Typography fontWeight={700}>{product.title}</Typography>
+          <Typography fontWeight={500}>${product.price}</Typography>
+      </Box>
     </Grid>
   );
 };

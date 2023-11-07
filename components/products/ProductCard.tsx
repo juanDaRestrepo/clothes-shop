18ec359613b5b2a8cc4f792ca88214a90922f8d5
Daniel Restrepo <jdrestrepo@unitecnica.net>
2023-11-07 12:13:04 -0500

    Finish description card part in productPage

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,46 +1,57 @@
+import {
+  Typography,
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  Box,
+  Link,
+} from "@mui/material";
+import { IProduct } from "../../interfaces";
+import { FC, useMemo, useState } from "react";
 
-
-import { Typography, Grid, Card, CardActionArea, CardMedia, Box } from '@mui/material';
-import { IProduct } from '../../interfaces';
-import { FC, useMemo, useState } from 'react';
+import NextLink from "next/link";
 
 interface Props {
-    product: IProduct;
+  product: IProduct;
 }
 
-const ProductCard: FC<Props> = ({product}) => {
-
+const ProductCard: FC<Props> = ({ product }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   const productImage = useMemo(() => {
     return isHovered
-      ? `products/${ product.images[1] }`
-      : `products/${ product.images[0] }`
-  }, [isHovered, product.images])
+      ? `products/${product.images[1]}`
+      : `products/${product.images[0]}`;
+  }, [isHovered, product.images]);
 
   return (
-    <Grid 
-      item 
-      xs={6} 
-      sm={4} 
+    <Grid
+      item
+      xs={6}
+      sm={4}
       key={product.slug}
-      onMouseEnter={ () => setIsHovered(true) }
-      onMouseLeave={ () => setIsHovered(false) }
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
       <Card>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            className='fadeIn'
-            image={productImage}
-            alt={product.title}
-          />
-        </CardActionArea>
+        <NextLink href="/product/slug" passHref prefetch={false} legacyBehavior>
+          <Link>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                className="fadeIn"
+                image={productImage}
+                alt={product.title}
+              />
+            </CardActionArea>
+          </Link>
+        </NextLink>
       </Card>
 
-      <Box sx={{mt: 1}} className='fadeIn'>
-          <Typography fontWeight={700}>{product.title}</Typography>
-          <Typography fontWeight={500}>${product.price}</Typography>
+      <Box sx={{ mt: 1 }} className="fadeIn">
+        <Typography fontWeight={700}>{product.title}</Typography>
+        <Typography fontWeight={500}>${product.price}</Typography>
       </Box>
     </Grid>
   );

096f34feb626c6c7828391ed64ed824a45dce5d5
Daniel Restrepo <jdrestrepo@unitecnica.net>
2023-11-14 15:20:12 -0500

    Show product info when image is loaded

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -18,7 +18,8 @@ interface Props {
 
 const ProductCard: FC<Props> = ({ product }) => {
   const [isHovered, setIsHovered] = useState(false);
-
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  
   const productImage = useMemo(() => {
     return isHovered
       ? `products/${product.images[1]}`
@@ -43,13 +44,14 @@ const ProductCard: FC<Props> = ({ product }) => {
                 className="fadeIn"
                 image={productImage}
                 alt={product.title}
+                onLoad={ () => setIsImageLoaded(true)}
               />
             </CardActionArea>
           </Link>
         </NextLink>
       </Card>
 
-      <Box sx={{ mt: 1 }} className="fadeIn">
+      <Box sx={{ mt: 1, display: isImageLoaded ? 'block' : 'none'  }} className="fadeIn">
         <Typography fontWeight={700}>{product.title}</Typography>
         <Typography fontWeight={500}>${product.price}</Typography>
       </Box>

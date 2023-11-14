5aa32165bf5b0f3249b1ea260c32e54b64a127fc
Daniel Restrepo <jdrestrepo@unitecnica.net>
2023-11-14 14:14:44 -0500

    Include swr for requesting data and use it in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,39 @@
-import type { NextPage } from 'next'
-import { Typography } from '@mui/material';
-import { ShopLayout } from '../components/layouts'
-import { initialData } from "../database/products"
-import ProductList from '../components/products/ProductCardList';
+import type { NextPage } from "next";
+
+import { Typography } from "@mui/material";
+import { ShopLayout } from "../components/layouts";
+import { initialData } from "../database/products";
+import ProductList from "../components/products/ProductCardList";
+
+import useSWR from "swr";
+const fetcher = (...args: [key: string]) =>
+  fetch(...args).then((res) => res.json());
 
 const Home: NextPage = () => {
+  const { data, error, isLoading } = useSWR("/api/products", fetcher);
+
+  console.log(data);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
   return (
-    <ShopLayout title={'The Clothe´s Shop- Home'} pageDescription={'Encuentra las mejores prendas de vestir y ropa deportiva'}>
-      <Typography variant='h1' component='h1'>Tienda</Typography>
-      <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
-      
-      <ProductList 
-        products={initialData.products as any}
-      />
+    <ShopLayout
+      title={"The Clothe´s Shop- Home"}
+      pageDescription={
+        "Encuentra las mejores prendas de vestir y ropa deportiva"
+      }
+    >
+      <Typography variant="h1" component="h1">
+        Tienda
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        Todos los productos
+      </Typography>
 
-
+      <ProductList products={data} />
     </ShopLayout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
